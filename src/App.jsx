617316c66d10c3022fa66436148399b1a9b8ff73
7lucky-axolotl228 <7error228@gmail.com>
2026-07-05 14:32:02 +0500

    Used react Routes to navigate through pages

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { Routes, Route, Navigate } from "react-router";
+import { useAuth } from "./context/AuthContext";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { LoginPage } from "./pages/LoginPage/LoginPage";
+import { ProfileHome } from "./pages/ProfilePages/ProfileHome";
+import { ProfileInfo } from "./pages/ProfilePages/ProfileInfo";
+import { ProfileLayout } from "./pages/ProfilePages/ProfileLayout";
+import { ProtectedRoute } from "./components/ProtectedRoute/ProtectedRoute";
+import "./App.css";
+
+function App() {
+
+    const { isAuthenticated, loading } = useAuth();
+
+    if (loading) {
+        return <p>Loading...</p>;
+    }
+
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={isAuthenticated ? <Navigate to="/profile" replace /> : <HomePage />}
+            />
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path="/profile" 
+                element={
+                    <ProtectedRoute>
+                        <ProfileLayout />
+                    </ProtectedRoute>
+                }
+            >
+                <Route path="/profile" element={<ProfileHome />} />
+                <Route path="info" element={<ProfileInfo />} />
+            </Route>
+
+        </Routes>
+    );
+}
+
+export default App;
