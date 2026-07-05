8a64c13f074cc9eccceefcbe0fc84bc38c6dd226
7lucky-axolotl228 <7error228@gmail.com>
2026-07-05 20:45:48 +0500

    Updated files into the one for each topic

---
+++ src/data/all-professions.js
@@ -0,0 +1,87 @@
+export let professionsAll = [
+    {
+        id: 1,
+        students: '294',
+        name: "Frontend - ReactJS",
+        image: "images/frontend-reactjs.webp",
+        price: "1500000 so'm",
+    },
+    {
+        id: 2,
+        students: '445',
+        name: "Frontend Dasturlash",
+        image: "images/frontend-dasturlash.webp",
+        price: "3000000 so'm",
+    },
+    {
+        id: 3,
+        students: '2424',
+        name: "Full Stack Python",
+        image: "images/full-stack-python.webp",
+        price: "3990000 so'm",
+    },
+    {
+        id: 4,
+        students: '2',
+        name: "Data sayns",
+        image: "images/data-science.webp",
+        price: "4990000 so'm",
+    },
+    {
+        id: 5,
+        students: '25',
+        name: "Kompyuter savodxonligi",
+        image: "images/komputer-savodxonligi.webp",
+        price: "499000 so'm",
+    },
+    {
+        id: 6,
+        students: '5',
+        name: "Odoo: funktional kengaytirish",
+        image: "images/odoo.webp",
+        price: "990000 so'm",
+    },
+    {
+        id: 7,
+        students: '1',
+        name: "Python: veb xaking vositalari",
+        image: "images/python-hacking.webp",
+        price: "990000 so'm",
+    },
+    {
+        id: 8,
+        students: '1',
+        name: "Python bilan hujumchi dasturlash",
+        image: "images/python-attacking.webp",
+        price: "990000 so'm",
+    },
+    {
+        id: 9,
+        students: '10',
+        name: "DevOps asoslari",
+        image: "images/devops.webp",
+        price: "3990000 so'm",
+    },
+    {
+        id: 10,
+        students: '3',
+        name: "Ma'lumotlar analitikasi",
+        image: "images/data-analytics.webp",
+        price: "3990000 so'm",
+    },
+];
+
+export let myProfessions = [
+    {
+        id: 1,
+        name: "Frontend - ReactJS",
+        image: "images/frontend-reactjs.webp",
+        progress: 75,
+    },
+    {
+        id: 2,
+        name: "Frontend Dasturlash",
+        image: "images/frontend-dasturlash.webp",
+        progress: 61,
+    },
+]
