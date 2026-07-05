7679080274ce0beb9275e1142e0a5b2e4cf24526
7lucky-axolotl228 <7error228@gmail.com>
2026-07-05 20:46:37 +0500

    Make the component a page for showing courses of the platform

--- src/pages/ProfilePages/ProfileCourses.jsx
+++ src/pages/ProfilePages/ProfileCourses.jsx
@@ -1,34 +1,12 @@
-import { courses } from "../../data/my-courses";
+import { AllCourses } from "../../components/AllCourses/AllCourses";
 
-export function ProfileCourses() {
+export function AllCoursesProfile() {
     return (
         <>
-            <div className="profs-grid">
-                {courses.map((course) => (
-                    <div key={course.id} className="prof-card">
-                        <div className="prof-card__image">
-                            <img src={course.image} alt={course.name} />
-                        </div>
-                        <div className="prof-card__body">
-                            <h3 className="prof-card__title">{course.name}</h3>
-                            <div className="prof-card__progress-wrap">
-                                <div className="prof-card__progress-bar">
-                                    <div
-                                        className="prof-card__progress-fill"
-                                        style={{ width: `${course.progress}%` }}
-                                    />
-                                </div>
-                                <span className="prof-card__percent">
-                                    {course.progress}%
-                                </span>
-                            </div>
-                            <button className="prof-card__btn">
-                                Darsni davom ettirish
-                            </button>
-                        </div>
-                    </div>
-                ))}
+            <div className="profile-page">
+                <h2 className="profile-section-title">Kurslar</h2>
+                <AllCourses />
             </div>
         </>
-    );
+    )
 }
