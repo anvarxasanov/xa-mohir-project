c3fae0d8e5081a55fd4197d9c1022d2355aebf60
7lucky-axolotl228 <7error228@gmail.com>
2026-07-05 14:34:41 +0500

    Add to js files with user's courses and proffessions information

---
+++ src/data/my-professions.js
@@ -0,0 +1,14 @@
+export let professions = [
+    {
+        id: 1,
+        name: "Frontend - ReactJS",
+        image: "images/frontend-reactjs.webp",
+        progress: 75,
+    },
+    {
+        id: 2,
+        name: "Frontend Dasturlash",
+        image: "images/frontend-dasturlash.webp",
+        progress: 61,
+    },
+]
