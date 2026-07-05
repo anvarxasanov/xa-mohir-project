08d8ae50e510151cdb50617c84f517d206ceef75
7lucky-axolotl228 <7error228@gmail.com>
2026-07-05 14:34:01 +0500

    Creates AuthContext to keep evety state in one place

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,46 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext(null);
+
+export function AuthProvider({ children }) {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const savedUser = localStorage.getItem('user');
+        if (savedUser) {
+            setUser(JSON.parse(savedUser));
+        }
+        setLoading(false);
+    }, []);
+
+    function login(email, password) {
+        if (email === 'khasanovanvar07@gmail.com' && password === '1234') {
+            const fakeUser = { email, password };
+            setUser(fakeUser);
+            localStorage.setItem('user', JSON.stringify(fakeUser));
+            return {success: true}
+        } else {
+            return {success: false, message: "Email or password in incorrect!"}
+        }
+    }
+
+    function logout() {
+        setUser(null);
+        localStorage.removeItem('user');
+    }
+
+    const value = {
+        user,
+        isAuthenticated: !!user,
+        loading,
+        login,
+        logout,
+    };
+
+    return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
+
+export function useAuth() {
+    return useContext(AuthContext);
+}
